2ebf6e138517f4e376dc0f097f37143f597a67fc
LianisDevs <lianisdevs@gmail.com>
2026-03-24 20:26:57 +1100

    fix: documentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
+"""This module contains add function with typescripting"""
+
+
 def add(a: float, b: float) -> float:
+    """two number addition function"""
     return a + b
