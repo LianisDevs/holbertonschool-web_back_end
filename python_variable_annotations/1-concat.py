328526aa2e631635cf64fdb486e81ed6fd9a5703
LianisDevs <lianisdevs@gmail.com>
2026-03-24 20:31:21 +1100

    add: task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module contains concat function with typescripting"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """two string concat function"""
+    return str1 + str2
