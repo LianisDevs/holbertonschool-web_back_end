a8d27b0ef02eb8bb88b91cb020dc0bc380dc0603
LianisDevs <lianisdevs@gmail.com>
2026-03-24 20:42:41 +1100

    add: task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module contains to_str function with typescripting"""
+
+
+def to_str(n: float) -> str:
+    """converts float to string function"""
+    return str(n)
