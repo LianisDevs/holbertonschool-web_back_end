0d97347d1bb71d17a2c6bb91f32633c01e9f5f28
LianisDevs <lianisdevs@gmail.com>
2026-03-31 22:15:57 +1100

    fix: handle if not int

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -5,12 +5,14 @@ import random
 import time
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """
     generates random number
     waits random number delay
     returns random number
     """
+    if not isinstance(max_delay, int):
+        return None
     random_num = random.uniform(0, max_delay)
     time.sleep(random_num)
     return random_num
