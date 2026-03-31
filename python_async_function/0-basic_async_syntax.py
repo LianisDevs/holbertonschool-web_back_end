883bfd110e7f1b3d2cc913641ae39a3a85e90f5f
LianisDevs <lianisdevs@gmail.com>
2026-03-31 21:46:33 +1100

    fix: typing added

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -5,12 +5,12 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10):
     """
     generates random number
     waits random number delay
     returns random number
     """
-    random_num = random.uniform(0, max_delay)
+    random_num: float = random.uniform(0, max_delay)
     await asyncio.sleep(random_num)
     return random_num
