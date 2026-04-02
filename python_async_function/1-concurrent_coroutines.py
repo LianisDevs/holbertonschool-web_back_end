09c0643b1cd04256aab2cea089df843e91c28d9c
LianisDevs <lianisdevs@gmail.com>
2026-04-02 17:54:27 +1100

    fix: early returns []

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -8,17 +8,17 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int, /) -> List[float] | None:
+async def wait_n(n: int, max_delay: int, /) -> List[float]:
     """
     uses async to call wait_random function n times
     returns list of random delay times in sorted list
     """
 
     if not isinstance(n, int) or not isinstance(max_delay, int):
-        return
+        return []
 
     if isinstance(n, bool) or isinstance(max_delay, bool):
-        return
+        return []
 
     return await asyncio.gather(*[
         asyncio.create_task(wait_random(max_delay)) for _ in range(n)])
