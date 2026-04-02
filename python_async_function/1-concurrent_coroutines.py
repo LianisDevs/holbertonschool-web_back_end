08452bd7fed9a790532f226755a238ef53e16f81
LianisDevs <lianisdevs@gmail.com>
2026-04-02 17:16:55 +1100

    fix: type check to include max_delay

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -5,14 +5,15 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list | None:
+async def wait_n(n: int, max_delay: int) -> list:
     """
     uses async to call wait_random function n times
     returns list of random delay times in sorted list
     """
 
-    if not isinstance(n, int) or isinstance(n, bool):
-        return
+    if not isinstance(n, int) or isinstance(n, bool) or \
+            isinstance(max_delay, bool):
+        return []
 
     delay_list = []
 
