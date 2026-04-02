50f1707208a6322dccb0cc565fb7c931459a8312
LianisDevs <lianisdevs@gmail.com>
2026-04-02 17:38:18 +1100

    fix: early return None

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -12,7 +12,7 @@ async def wait_n(n: int, max_delay: int, /) -> list[float]:
     """
 
     if not isinstance(n, int) or not isinstance(max_delay, int):
-        return []
+        return
 
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     return [await completed for completed in asyncio.as_completed(tasks)]
