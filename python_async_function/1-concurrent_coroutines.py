68ada3540ddf5976ff3cc1e4ff7436e3f26c12f1
LianisDevs <lianisdevs@gmail.com>
2026-04-02 17:26:48 +1100

    fix: enforce argument order

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -5,7 +5,7 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int, /) -> list:
     """
     uses async to call wait_random function n times
     returns list of random delay times in sorted list
