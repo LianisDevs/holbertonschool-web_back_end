59141547bce9c7f753d9b7fd247587866b325ebd
LianisDevs <lianisdevs@gmail.com>
2026-04-02 17:33:05 +1100

    fix: return type list[float]

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -5,14 +5,14 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int, /) -> list | None:
+async def wait_n(n: int, max_delay: int, /) -> list[float]:
     """
     uses async to call wait_random function n times
     returns list of random delay times in sorted list
     """
 
     if not isinstance(n, int):
-        return
+        return []
 
     delay_list = []
 
