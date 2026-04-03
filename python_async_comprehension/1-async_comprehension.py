7f3f748a220cb975fc48b6e5b54ea8bfd8279a83
LianisDevs <lianisdevs@gmail.com>
2026-04-03 14:37:28 +1100

    add: task 1

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+async_comprehension module
+"""
+
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    async_comprehension()
+    using asyn comprehension to return the yields from
+    async_generator()
+    """
+    return [i async for i in async_generator()]
