ad805d863e47b517bc0fe5bdedb15aebe1e42a53
LianisDevs <lianisdevs@gmail.com>
2026-04-14 19:10:13 +1000

    update: assert within get_hyper()

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -6,16 +6,7 @@ Contains Server Class- with methods: dataset(), index_range() and get_page()
 
 import csv
 import math
-from typing import List, Tuple, TypedDict
-
-
-class HyperReponse(TypedDict):
-    page_size: int
-    page: int
-    data: List[List]
-    next_page: int | None
-    prev_page: int | None
-    total_pages: int
+from typing import Any, Dict, List, Tuple
 
 
 class Server:
@@ -64,11 +55,15 @@ class Server:
 
         return data[start:end]
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> HyperReponse:
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
         """
         Parameters: page and page_size
         Return: appropriate page of the dataset e.g correct list of rows
         """
+
+        assert (isinstance(page, int) and page > 0)
+        assert (isinstance(page_size, int) and page_size > 0)
+
         returned_data = self.get_page(page, page_size)
 
         data = self.dataset()
