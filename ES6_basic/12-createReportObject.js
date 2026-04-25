0187aa275e42ff57fa30c33eb7051b97993d95c0
LianisDevs <lianisdevs@gmail.com>
2026-04-25 14:51:38 +1000

    add: task 12

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,10 @@
+export default function createReportObject(employeesList) {
+  const employeeObj = {
+    allEmployees: employeesList,
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+
+  };
+  return employeeObj;
+}
