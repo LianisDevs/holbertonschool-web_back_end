7cb0ca81a50fe4deacfe0c02383634d6c1566161
LianisDevs <lianisdevs@gmail.com>
2026-04-25 18:54:39 +1000

    add: task 4

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,34 @@
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (Number.isInteger(value) !== true) throw new TypeError('Amount must be a number');
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPric(amount, conversionRate) {
+    if (Number.isInteger(amount) !== true) throw new TypeError('Amount must be a number');
+
+    if (Number.isInteger(conversionRate) !== true) throw new TypeError('Conversion rate must be a number');
+    return amount * conversionRate;
+  }
+}
