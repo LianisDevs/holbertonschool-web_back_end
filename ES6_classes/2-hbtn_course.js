548551b587256c75d33fbd46eeaf9b94e5ddf6cc
LianisDevs <lianisdevs@gmail.com>
2026-04-25 17:46:49 +1000

    add: task 2

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,35 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') throw new TypeError('Name must be a string');
+    this._name = value;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    if (Number.isInteger(value) === false) throw new TypeError('Length must be a number');
+    this._length = value;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    const isStringArray = Array.isArray(value) && value.every((item) => typeof item === 'string');
+    if (isStringArray === false) throw new TypeError('Students must be an array of strings');
+    this._students = value;
+  }
+}
