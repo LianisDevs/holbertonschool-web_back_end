55036efb9ae7b1b18134463839eb3745616d88aa
LianisDevs <lianisdevs@gmail.com>
2026-05-04 08:04:42 +1000

    fix: log err message

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -5,5 +5,7 @@ export default function handleProfileSignup() {
     .then(([photoResponse, userResponse]) => {
       console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
     })
-    .catch(() => new Error('Signup system offline'));
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
