49f1f1e5070d508b3a9e223b60e5d029aae02b41
LianisDevs <lianisdevs@gmail.com>
2026-05-05 18:47:35 +1000

    fix: print close message only if not a TTY

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -9,7 +9,8 @@ console.log('Welcome to Holberton School, what is your name?');
 
 rl.on('line', (name) => {
   console.log(`Your name is: ${name}`);
-  rl.close();
+
+  if (!process.stdin.isTTY) rl.close();
 });
 
 rl.on('close', () => {
