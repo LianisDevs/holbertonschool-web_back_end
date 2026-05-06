42459c940146d441c0acc0d06e0f16845dcc4139
LianisDevs <lianisdevs@gmail.com>
2026-05-06 10:29:15 +1000

    fix: restructure

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,10 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-let data = '';
-
-process.stdin.on('data', (chunk) => {
-  data += chunk;
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  process.stdout.write(`Your name is: ${name}\n`);
 });
 
 process.stdin.on('end', () => {
-  const name = data.trim();
-  process.stdout.write(`Your name is: ${name}\n`);
   process.stdout.write('This important software is now closing\n');
 });
