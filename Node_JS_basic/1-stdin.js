4d730c1a64d63b24d2312f17185b50ab2a17efd8
LianisDevs <lianisdevs@gmail.com>
2026-05-06 21:13:44 +1000

    fix: ctrl+d

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,19 +3,19 @@ let input = '';
 console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
+  input += data.toString();
+
   if (process.stdin.isTTY) {
-    const name = data.toString().trim();
+    const name = input.trim();
     if (name.length > 0) {
-      process.stdout.write(`Your name is: ${name}\r\n`);
+      process.stdout.write(`Your name is: ${name}\r`);
     }
-  } else {
-    input += data.toString();
   }
 });
 
 process.stdin.on('end', () => {
-  if (!process.stdin.isTTY) {
-    const name = input.trim();
+  const name = input.trim();
+  if (name.length > 0) {
     process.stdout.write(`Your name is: ${name}\n`);
   }
   process.stdout.write('This important software is now closing\n');
