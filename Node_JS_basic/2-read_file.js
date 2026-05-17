27ea7826cc8640f078b95400fb801a5f63fa6076
LianisDevs <lianisdevs@gmail.com>
2026-05-17 19:58:19 +1000

    add: task 2

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,30 @@
+const fs = require('node:fs');
+
+module.exports = function countStudents(filePath) {
+  try {
+    const data = fs.readFileSync(filePath, 'utf8');
+
+    const lines = data.split('\n').map((row) => row.split(','));
+
+    const students = lines.slice(1);
+
+    const csList = [];
+    const sweList = [];
+
+    for (const innerArray of students) {
+      const last = innerArray[innerArray.length - 1];
+
+      if (last === 'CS') {
+        csList.push(innerArray[0]);
+      } else {
+        sweList.push(innerArray[0]);
+      }
+    }
+
+    console.log('Number of students:', students.length);
+    console.log(`Number of students in CS: ${csList.length}. List: ${csList.join(', ')}`);
+    console.log(`Number of students in SWE: ${sweList.length}. List: ${sweList.join(', ')}`);
+  } catch (err) {
+    console.log('Cannot load the database');
+  }
+};
